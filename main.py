a2386b2e7972840e226b14505a7cb4538c85288a
MalakLH <malak.lahyani@supcom.tn>
2026-09-07 14:58:21 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+
+with open('presentation.html', 'r', encoding='utf-8') as html_file:
+    html_content = html_file.read()
+    print(html_content) # Print the HTML content to verify it's read correctly
+
+    soup = BeautifulSoup(html_content, 'lxml')
+    print(soup.prettify()) # Print the prettified HTML to verify BeautifulSoup parsing
+
+    tags = soup.find_all('h3')
+    print(tags) # Print all <h3> tags found in the HTML, we get a list as a result that has all the <h3> tags in it. We can iterate over this list to get each tag individually.
