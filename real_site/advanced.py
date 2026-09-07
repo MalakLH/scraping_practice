dd3479f2c6cff7bfd5290017ec8863e9205d5c51
MalakLH <malak.lahyani@supcom.tn>
2026-09-07 18:09:32 +0200

    made scraping a function

--- real_site/advanced.py
+++ real_site/advanced.py
@@ -1,40 +1,41 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_content = requests.get('https://www.panda.tn/index.php?id_product=126&id_product_attribute=0&rewrite=pack-creme-eclat-a-l-huile-de-sesame&controller=product').text
+def scrape_product_info(url):
+    html_content = requests.get(url).text
 
-soup = BeautifulSoup(html_content, 'lxml')
-charts = soup.find_all('div', class_='row')
-
-
-for chart in charts:
-
-    try:
-        produit = chart.find('h1').text.replace('  ','').replace('\n','')
-        print(f'Produit: {produit}')
-        break
-
-    except AttributeError:
-        pass
+    soup = BeautifulSoup(html_content, 'lxml')
+    charts = soup.find_all('div', class_='row')
 
 
-for chart in charts:
+    for chart in charts:
 
-    try:
-        prix = chart.find('div', class_="product-prices").text.replace('  ','').replace('\n','').replace('\t','')
-        print(f'Prix: {prix}')
-        break
+        try:
+            produit = chart.find('h1').text.replace('  ','').replace('\n','')
+            print(f'Produit: {produit}')
+            break
 
-    except AttributeError:
-        pass
+        except AttributeError:
+            pass
 
 
-for chart in charts:
-    
-    try:
-        description = chart.find('p').text.replace('  ','').replace('\n','').replace('\t','')
-        print(f'Description: {description}')
-        break
+    for chart in charts:
 
-    except AttributeError:
-        pass
+        try:
+            prix = chart.find('div', class_="product-prices").text.replace('  ','').replace('\n','').replace('\t','')
+            print(f'Prix: {prix}')
+            break
+
+        except AttributeError:
+            pass
+
+
+    for chart in charts:
+        
+        try:
+            description = chart.find('p').text.replace('  ','').replace('\n','').replace('\t','')
+            print(f'Description: {description}')
+            break
+
+        except AttributeError:
+            pass
